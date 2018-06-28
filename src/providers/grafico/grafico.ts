43e1433e5c2e2a38256754eba456ce79b56c4be8
bladerangel <bladerangel10@gmail.com>
2018-06-28 17:36:41 -0300

    refatorando gráfico

--- src/providers/grafico/grafico.ts
+++ src/providers/grafico/grafico.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import Chart from 'chart.js';
+import '../../lib/Chart.Bands';
 
 @Injectable()
 export class GraficoProvider {
@@ -20,31 +21,42 @@ export class GraficoProvider {
   }
 
   getLinha(dados) {
+    const cores = dados.map((numero) => numero.valor > 0 ? '#009688' : '#f44336');
     const data = {
       labels: dados.map((numero) => numero.ano),
       datasets: [{
         label: '(R$ - Milhões)',
-        backgroundColor: 'rgb(0, 0, 255)',
-        borderColor: 'rgb(0, 0, 255)',
+        lineTension: 0,
+        borderColor: '#8BC34A',
+        backgroundColor: '#8BC34A',
+        pointBorderColor: cores,
+        pointBackgroundColor: cores,
+        pointRadius : 5,
+        pointHoverRadius: 10,
         data: dados.map((numero) => numero.valor),
         fill: false
       }]
     };
 
-
     const opcoes = {
-      responsive: true,
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: '(R$ - Milhões)'
+          }
+        }]
+      },
+      legend: {
+        display: false
+      },
+      bands: {
+        yValue: 0,
+        belowThresholdColour: ['#FF5722']
+      },
       title: {
         display: true,
         text: 'Histórico do Lucro Líquido'
-      },
-      tooltips: {
-        mode: 'index',
-        intersect: false,
-      },
-      hover: {
-        mode: 'nearest',
-        intersect: true
       }
     };
 
