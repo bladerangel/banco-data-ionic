9e153707a698bae4cd8ed117ece4599c95f275d5
bladerangel <bladerangel10@gmail.com>
2018-06-30 14:49:03 -0300

    usando modal para mostrar relatório

--- src/pages/relatorio/relatorio.ts
+++ src/pages/relatorio/relatorio.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import { IonicPage, NavParams, ViewController } from 'ionic-angular';
 
 import { GraficoProvider } from '../../providers/grafico/grafico';
 
@@ -13,13 +13,17 @@ export class RelatorioPage implements OnInit {
   relatorio;
   informacoes;
 
-  constructor(private navParams: NavParams, private graficoProvider: GraficoProvider) {
+  constructor(private navParams: NavParams, private graficoProvider: GraficoProvider, private viewCtrl: ViewController) {
   }
 
   ngOnInit() {
     this.relatorio = this.navParams.get('relatorio');
     this.informacoes = Object.keys(this.relatorio.sobre.informacoes);
     this.graficoProvider.getLinha('lucroLiquidoAnual', this.relatorio.lucroLiquidoAnual.valores.reverse());
+  }
+
+  fechar() {
+    this.viewCtrl.dismiss();
   }
 
 }
